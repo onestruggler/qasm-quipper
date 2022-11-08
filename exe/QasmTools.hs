215205357554de83d54532477621f28c11c16949
scottwe <aswesley@uwaterloo.ca>
2022-11-08 13:27:06 -0400

    Integrated parser with qasm_tools

--- exe/QasmTools.hs
+++ exe/QasmTools.hs
@@ -2,20 +2,24 @@
 
 {-# LANGUAGE RecordWildCards #-}
 
+module Main where
+
 import IOUtils (readSrc, withOut)
-import Qasm.AST (AstStmt)
-import Qasm.Language (Stmt)
-import Qasm.Parser (parseQasm)
 import QasmCmdLn (QasmTools(..), getToolArgs)
+import Qasm.AST (AstStmt)
+import Qasm.Parser (parseQasm)
+import System.Exit (die)
 import System.IO (Handle, hPutStrLn)
+import Text.Pretty.Simple (pHPrint)
 
 -------------------------------------------------------------------------------
 -- * Utility Types
 
 -- | A function that takes as input the contents of an OpenQASM 3 file, and
--- returns an abstract representation for the program of type a. The second
--- string indicates the input source, and is used for error logging.
-type DoTaskFn a = String -> String -> a
+-- returns either an abstract representation for the program of type a, or a
+-- parsing error in the form of a string. The first string indicates the input
+-- source, and is used for error logging.
+type DoTaskFn a = String -> String -> Either String a
 
 -- | A function that takes as input a file handle and the abstraction
 -- represerntation of an OpenQASM 3 program of type a. The abstract
@@ -23,19 +27,10 @@ type DoTaskFn a = String -> String -> a
 type DisplayFn a = Handle -> a -> IO ()
 
 -------------------------------------------------------------------------------
--- * Parser Interface.
-
-parse :: DoTaskFn [Stmt]
-parse text file = []
-
-displayAST :: DisplayFn [Stmt]
-displayAST hdl ast = hPutStrLn hdl "Parser."
-
--------------------------------------------------------------------------------
 -- * Analyzer Interface.
 
 analyze :: DoTaskFn [AstStmt]
-analyze text file = []
+analyze text file = Right []
 
 displayIR :: DisplayFn [AstStmt]
 displayIR hdl ast = hPutStrLn hdl "Analyzer."
@@ -44,7 +39,7 @@ displayIR hdl ast = hPutStrLn hdl "Analyzer."
 -- * Writer Interface.
 
 codegen :: Bool -> DoTaskFn [String]
-codegen legacy text file = []
+codegen legacy text file = Right []
 
 displayQasm :: DisplayFn [String]
 displayQasm hdl lines = hPutStrLn hdl "Codegen."
@@ -57,7 +52,10 @@ displayQasm hdl lines = hPutStrLn hdl "Codegen."
 -- source file (file), and the handle to an output file (outHdl). Processes the
 -- text using doTask, and displays the results to outHdl using display.
 runTool :: DoTaskFn a -> DisplayFn a -> String -> String -> Handle -> IO ()
-runTool doTask display text file outHdl = display outHdl $ doTask text file
+runTool doTask display text file outHdl =
+    case doTask file text of
+        Left err  -> die err
+        Right rep -> display outHdl rep
 
 -- | Takes an input a tool task (doTask), a function to display the results
 -- (display), an input source (src), and an output destination (dst). Reads all
@@ -71,7 +69,7 @@ setupTool doTask display src out = do
 -- | Takes an input an instance of QasmTools arguments. The arguments are
 -- dispatched to the correct invocation of setupTool.
 processArgs :: QasmTools -> IO ()
-processArgs mode@Parser{..}   = setupTool parse displayAST src out
+processArgs mode@Parser{..}   = setupTool parseQasm pHPrint src out
 processArgs mode@Analyzer{..} = setupTool analyze displayIR src out
 processArgs mode@Writer{..}   = setupTool doTaskFn displayQasm src out
     where doTaskFn = codegen legacy
