4d729853f585f5d04680b3772ecd1d407858a5bf
scottwe <aswesley@uwaterloo.ca>
2022-12-02 22:40:53 -0400

    Types to represent Quipper gate names

---
+++ src/Quip/GateName.hs
@@ -0,0 +1,58 @@
+-- | Named gates and associated data.
+
+module Quip.GateName where
+
+-------------------------------------------------------------------------------
+-- * Gate Name Declarations.
+
+-- | Assigns a unique type to each standard named gate in Quipper.
+data GateName = GateX
+              | GateY
+              | GateZ
+              | GateH
+              | GateSwap
+              | GateV
+              | GateS
+              | GateT
+              | GateIX
+              | GateOmega
+              | GateW
+              | GateE
+              | UserDefinedGate String
+              deriving (Show, Eq)
+
+-- | Assigns a unique type to each standard rotation gate in Quipper.
+--
+-- Note: Both RotExpZ and RotZ are both e^(-iZt), up to a global phase. While
+-- global phase is unobservation, the difference in phase is observable when
+-- both RotExpZ and RotZ are controlled.    
+data RotName = RotExpZ
+             | RotZ
+             | UserDefinedRot String
+             deriving (Show, Eq)
+
+-------------------------------------------------------------------------------
+-- * Name Analysis Functions.
+
+-- | Interprets a string as a standard Quipper gate name.
+toGateName :: String -> GateName
+toGateName "not"   = GateX
+toGateName "X"     = GateX
+toGateName "Y"     = GateY
+toGateName "Z"     = GateZ
+toGateName "H"     = GateH
+toGateName "SWAP"  = GateSwap
+toGateName "V"     = GateV
+toGateName "S"     = GateS
+toGateName "T"     = GateT
+toGateName "iX"    = GateIX
+toGateName "Omega" = GateOmega
+toGateName "W"     = GateW
+toGateName "E"     = GateE
+toGateName str     = UserDefinedGate str
+
+-- | Interprets a string as a standard Quipper rotation name.
+toRotName :: String -> RotName
+toRotName "exp(-i%Z)" = RotExpZ
+toRotName "R(2pi/%)"  = RotZ
+toRotName str         = UserDefinedRot str
