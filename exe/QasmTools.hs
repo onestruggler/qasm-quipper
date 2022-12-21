807ccb57a8ef7cc8e83a23dd5acba4072decc1f9
scottwe <aswesley@uwaterloo.ca>
2022-12-21 11:11:09 -0400

    Factored out task framework from QasmTools

--- exe/QasmTools.hs
+++ exe/QasmTools.hs
@@ -4,29 +4,14 @@
 
 module Main where
 
-import IOUtils (readSrc, withOut)
 import QasmCmdLn (QasmTools(..), getToolArgs)
 import Qasm.AST (AstStmt)
 import Qasm.Parser (parseQasm)
 import Qasm.Passes (toAst, elimInv, elimPow)
 import Qasm.Printer (printAst)
-import System.Exit (die)
-import System.IO (Handle, hPutStrLn)
+import System.IO (hPutStrLn)
+import SetupTools (DoTaskFn, DisplayFn, setupTool)
 import Text.Pretty.Simple (pHPrint)
-
--------------------------------------------------------------------------------
--- * Utility Types
-
--- | A function that takes as input the contents of an OpenQASM 3 file, and
--- returns either an abstract representation for the program of type a, or a
--- parsing error in the form of a string. The first string indicates the input
--- source, and is used for error logging.
-type DoTaskFn a = String -> String -> Either String a
-
--- | A function that takes as input a file handle and the abstraction
--- represerntation of an OpenQASM 3 program of type a. The abstract
--- representation is displayed to the given handle.
-type DisplayFn a = Handle -> a -> IO ()
 
 -------------------------------------------------------------------------------
 -- * Analyzer Interface.
@@ -70,26 +55,7 @@ displayQasm hdl (line:lines) = do
 -------------------------------------------------------------------------------
 -- * Entry Point.
 
--- | Takes an input a tool task (doTask), a function to display the results
--- (display), the source text of an OpenQASM 3 program (text), the name of the
--- source file (file), and the handle to an output file (outHdl). Processes the
--- text using doTask, and displays the results to outHdl using display.
-runTool :: DoTaskFn a -> DisplayFn a -> String -> String -> Handle -> IO ()
-runTool doTask display text file outHdl =
-    case doTask file text of
-        Left err  -> die err
-        Right rep -> display outHdl rep
-
--- | Takes an input a tool task (doTask), a function to display the results
--- (display), an input source (src), and an output destination (dst). Reads all
--- contents from source, acquires a handle for dst, and then dispatches doTask,
--- display, the contents, and the handle to runTool.
-setupTool :: DoTaskFn a -> DisplayFn a -> String -> String -> IO ()
-setupTool doTask display src out = do
-    (text, file) <- readSrc src
-    withOut out (runTool doTask display text file)
-
--- | Takes an input an instance of QasmTools arguments. The arguments are
+-- | Takes as input an instance of QasmTools arguments. The arguments are
 -- dispatched to the correct invocation of setupTool.
 processArgs :: QasmTools -> IO ()
 processArgs mode@Parser{..}   = setupTool parseQasm pHPrint src out
---
+++ exe/SetupTools.hs
@@ -0,0 +1,47 @@
+-- | General-purpose functions to pass along source files to tools.
+
+module SetupTools
+  ( DoTaskFn
+  , DisplayFn
+  , setupTool
+  ) where
+
+import IOUtils (readSrc, withOut)
+import System.Exit (die)
+import System.IO (Handle)
+
+-------------------------------------------------------------------------------
+-- * Tool Wrappers
+
+-- | A function that takes as input the contents of a source 3 file, and
+-- returns either an abstract representation for the program of type a, or a
+-- parsing error in the form of a string. The first string indicates the input
+-- source, and is used for error logging.
+type DoTaskFn a = String -> String -> Either String a
+
+-- | A function that takes as input a file handle and the abstraction
+-- represerntation of an OpenQASM 3 program of type a. The abstract
+-- representation is displayed to the given handle.
+type DisplayFn a = Handle -> a -> IO ()
+
+-------------------------------------------------------------------------------
+-- * Entry Point
+
+-- | Takes an input a tool task (doTask), a function to display the results
+-- (display), the source text of an OpenQASM 3 program (text), the name of the
+-- source file (file), and the handle to an output file (outHdl). Processes the
+-- text using doTask, and displays the results to outHdl using display.
+runTool :: DoTaskFn a -> DisplayFn a -> String -> String -> Handle -> IO ()
+runTool doTask display text file outHdl =
+    case doTask file text of
+        Left err  -> die err
+        Right rep -> display outHdl rep
+
+-- | Takes an input a tool task (doTask), a function to display the results
+-- (display), an input source (src), and an output destination (dst). Reads all
+-- contents from source, acquires a handle for dst, and then dispatches doTask,
+-- display, the contents, and the handle to runTool.
+setupTool :: DoTaskFn a -> DisplayFn a -> String -> String -> IO ()
+setupTool doTask display src out = do
+    (text, file) <- readSrc src
+    withOut out (runTool doTask display text file)
