742ccd65c32c7f6980ddfc6f6f38cdf229a5f464
scottwe <aswesley@uwaterloo.ca>
2023-01-03 12:57:09 -0400

    Partial impl of a map from var decls to wires (1).
    This commit supports decl allocation and types.

--- src/QasmToQuip/Wire.hs
+++ src/QasmToQuip/Wire.hs
@@ -4,10 +4,15 @@
 module QasmToQuip.Wire
   ( WireState
   , WireError(..)
+  , WireAllocMap
+  , DeclType(..)
   , Show(..)
   , Eq(..)
+  , allocate
   , allocateWire
+  , getDeclType
   , isInput
+  , initialAllocations
   , initWire
   , isOutput
   , termWire
@@ -18,10 +23,13 @@ module QasmToQuip.Wire
 -------------------------------------------------------------------------------
 -- * Import Section.
 
+import qualified Data.IntMap.Strict as IntMap
+import qualified Data.Map.Strict as Map
 import Data.Maybe
   ( isJust
   , maybe
   )
+import Quip.Wire (WireType)
 import Utils (setMaybe)
 
 -------------------------------------------------------------------------------
@@ -78,7 +86,7 @@ updateWire init term state
     | not init && not active = Left UseBeforeInit
     -- Otherwise, update the state.
     | otherwise = Right WireState { wireIndex = wireIndex state
-                                  , initFirst = setMaybe init (initFirst state)
+                                  , initFirst = setMaybe init $ initFirst state
                                   , termAtEnd = term
                                   }
     where active = isOutput state
@@ -98,3 +106,61 @@ initWire = updateWire True False
 -- use, then an error is returned.
 termWire :: WireUpdate
 termWire = updateWire False True
+
+-------------------------------------------------------------------------------
+-- * Declaration to Wire Mappings.
+
+-- | Helper type to represent the WireState of a declaration (in particular,
+-- either a WireState or a mapping from array indices to WireStates).
+type DeclWireState = Either WireState (IntMap.IntMap WireState)
+
+-- | Helper type to map declaration names to DeclWireStates.
+type DeclWireStateMap = Map.Map String (WireType, DeclWireState)
+
+-- | Maintains a mapping from declarartions to WireStates.
+data WireAllocMap = WireAllocMap Int DeclWireStateMap deriving (Eq, Show)
+
+-- | Helper type for fuctions taht try to update WireAllocMaps, with the
+-- possibility of failure.
+type TryMapUpdate = WireAllocMap -> Maybe WireAllocMap
+
+-- | A enum to indicate whether a declaration is undeclared, or a scalar-type,
+-- or of an array-type.
+data DeclType = Undeclared
+              | Scalar WireType
+              | Array WireType Int
+              deriving (Eq, Show)
+
+-- | Returns an empty wire allocation map.
+initialAllocations :: WireAllocMap
+initialAllocations = WireAllocMap 0 Map.empty
+
+-- | Helper function to allocate wires of an array.
+populateWires :: Int -> Int -> IntMap.IntMap WireState
+populateWires wct size = foldr f IntMap.empty [0..(size - 1)]
+    where f idx = IntMap.insert idx (allocateWire $ size + idx)
+
+-- | Takes as input a declaration identifier (name), the declaration size when
+-- applicable (size), and a wire allocation map. If (name, size) describes a
+-- scalar declaration (i.e., size is Nothing) not found in the map, then adds a
+-- new wire to the map associated to name. If (name, size) describes an array
+-- declaration (i.e., size is Just n) not found in the map, then adds new wires
+-- to the map associated to each of name[0] through to name[n-1]. Otherwise,
+-- nothing is returned.
+allocate :: WireType -> String -> (Maybe Int) -> TryMapUpdate
+allocate ty name size (WireAllocMap n map) =
+    if Map.member name map
+    then Nothing
+    else case size of
+        Nothing -> add 1 (Left $ allocateWire n)
+        Just m  -> add m (Right $ populateWires n m)
+    where add c st = Just $ WireAllocMap (n + c) (Map.insert name (ty, st) map)
+
+-- | Takes as input a declaration name and a wire allocation map. Returns the
+-- type of the declaration, according to the declaration map.
+getDeclType :: String -> WireAllocMap -> DeclType
+getDeclType name (WireAllocMap _ map) =
+    case Map.lookup name map of
+        Nothing                -> Undeclared
+        Just (ty, Left _)      -> Scalar ty
+        Just (ty, Right cells) -> Array ty (IntMap.size cells)
--- test/QasmToQuip-Wire-Tests.hs
+++ test/QasmToQuip-Wire-Tests.hs
@@ -1,8 +1,10 @@
 module Main where
 
+import Data.Maybe
 import Test.Framework
 import Test.Framework.Providers.HUnit
 import Test.HUnit
+import Quip.Wire
 import QasmToQuip.Wire
 
 -----------------------------------------------------------------------------------------
@@ -162,6 +164,139 @@ test37 = TestCase (assertEqual "WireState API: wireIndex (6/6)."
                                (wireIndex state0'))
 
 -----------------------------------------------------------------------------------------
+-- Wire Allocation Map Initialization.
+
+alloc0 = initialAllocations
+alloc1 = fromJust $ allocate QWire "decl1" Nothing  alloc0
+alloc2 = fromJust $ allocate CWire "decl2" (Just 5) alloc1
+alloc3 = fromJust $ allocate QWire "decl3" (Just 4) alloc2
+alloc4 = fromJust $ allocate CWire "decl4" Nothing  alloc3
+
+test38 = TestCase (assertEqual "WireAllocMap: initialization (1/5)."
+                               Undeclared
+                               (getDeclType "decl1" alloc0))
+
+test39 = TestCase (assertEqual "WireAllocMap: initialization (2/5)."
+                               Undeclared
+                               (getDeclType "decl2" alloc0))
+
+test40 = TestCase (assertEqual "WireAllocMap: initialization (3/5)."
+                               Undeclared
+                               (getDeclType "decl3" alloc0))
+
+test41 = TestCase (assertEqual "WireAllocMap: initialization (4/5)."
+                               Undeclared
+                               (getDeclType "decl4" alloc0))
+
+test42 = TestCase (assertEqual "WireAllocMap: initialization (5/5)."
+                               Undeclared
+                               (getDeclType "decl5" alloc0))
+
+test43 = TestCase (assertEqual "WireAllocMap: allocation (1/20)."
+                               (Scalar QWire)
+                               (getDeclType "decl1" alloc1))
+
+test44 = TestCase (assertEqual "WireAllocMap: allocation (2/20)."
+                               Undeclared
+                               (getDeclType "decl2" alloc1))
+
+test45 = TestCase (assertEqual "WireAllocMap: allocation (3/20)."
+                               Undeclared
+                               (getDeclType "decl3" alloc1))
+
+test46 = TestCase (assertEqual "WireAllocMap: allocation (4/20)."
+                               Undeclared
+                               (getDeclType "decl4" alloc1))
+
+test47 = TestCase (assertEqual "WireAllocMap: allocation (5/20)."
+                               Undeclared
+                               (getDeclType "decl5" alloc1))
+
+test48 = TestCase (assertEqual "WireAllocMap: allocation (6/20)."
+                               (Scalar QWire)
+                               (getDeclType "decl1" alloc2))
+
+test49 = TestCase (assertEqual "WireAllocMap: allocation (7/20)."
+                               (Array CWire 5)
+                               (getDeclType "decl2" alloc2))
+
+test50 = TestCase (assertEqual "WireAllocMap: allocation (8/20)."
+                               Undeclared
+                               (getDeclType "decl3" alloc2))
+
+test51 = TestCase (assertEqual "WireAllocMap: allocation (9/20)."
+                               Undeclared
+                               (getDeclType "decl4" alloc2))
+
+test52 = TestCase (assertEqual "WireAllocMap: allocation (10/20)."
+                               Undeclared
+                               (getDeclType "decl5" alloc2))
+
+test53 = TestCase (assertEqual "WireAllocMap: allocation (11/20)."
+                               (Scalar QWire)
+                               (getDeclType "decl1" alloc3))
+
+test54 = TestCase (assertEqual "WireAllocMap: allocation (12/20)."
+                               (Array CWire 5)
+                               (getDeclType "decl2" alloc3))
+
+test55 = TestCase (assertEqual "WireAllocMap: allocation (13/20)."
+                               (Array QWire 4)
+                               (getDeclType "decl3" alloc3))
+
+test56 = TestCase (assertEqual "WireAllocMap: allocation (14/20)."
+                               Undeclared
+                               (getDeclType "decl4" alloc3))
+
+test57 = TestCase (assertEqual "WireAllocMap: allocation (15/20)."
+                               Undeclared
+                               (getDeclType "decl5" alloc3))
+
+test58 = TestCase (assertEqual "WireAllocMap: allocation (16/20)."
+                               (Scalar QWire)
+                               (getDeclType "decl1" alloc4))
+
+test59 = TestCase (assertEqual "WireAllocMap: allocation (17/20)."
+                               (Array CWire 5)
+                               (getDeclType "decl2" alloc4))
+
+test60 = TestCase (assertEqual "WireAllocMap: allocation (18/20)."
+                               (Array QWire 4)
+                               (getDeclType "decl3" alloc4))
+
+test61 = TestCase (assertEqual "WireAllocMap: allocation (19/20)."
+                               (Scalar CWire)
+                               (getDeclType "decl4" alloc4))
+
+test62 = TestCase (assertEqual "WireAllocMap: allocation (20/20)."
+                               Undeclared
+                               (getDeclType "decl5" alloc4))
+
+test63 = TestCase (assertEqual "WireAllocMap: double initialize (1/6)."
+                               Nothing
+                               (allocate QWire "decl1" Nothing alloc1))
+
+test64 = TestCase (assertEqual "WireAllocMap: double initialize (2/6)."
+                               Nothing
+                               (allocate QWire "decl1" Nothing alloc2))
+
+test65 = TestCase (assertEqual "WireAllocMap: double initialize (3/6)."
+                               Nothing
+                               (allocate QWire "decl1" Nothing alloc4))
+
+test66 = TestCase (assertEqual "WireAllocMap: double initialize (4/6)."
+                               Nothing
+                               (allocate QWire "decl2" Nothing alloc4))
+
+test67 = TestCase (assertEqual "WireAllocMap: double initialize (5/6)."
+                               Nothing
+                               (allocate QWire "decl3" Nothing alloc4))
+
+test68 = TestCase (assertEqual "WireAllocMap: double initialize (6/6)."
+                               Nothing
+                               (allocate QWire "decl4" Nothing alloc4))
+
+-----------------------------------------------------------------------------------------
 -- Orchestrates tests.
 
 tests = hUnitTestToTests $ TestList [TestLabel "WireAPI_UniqueStates_1" test1,
@@ -200,6 +335,37 @@ tests = hUnitTestToTests $ TestList [TestLabel "WireAPI_UniqueStates_1" test1,
                                      TestLabel "WireAPI_WireIndex_3" test34,
                                      TestLabel "WireAPI_WireIndex_4" test35,
                                      TestLabel "WireAPI_WireIndex_5" test36,
-                                     TestLabel "WireAPI_WireIndex_6" test37]
+                                     TestLabel "WireAPI_WireIndex_6" test37,
+                                     TestLabel "WireAllocMap_Init_1" test38,
+                                     TestLabel "WireAllocMap_Init_2" test39,
+                                     TestLabel "WireAllocMap_Init_3" test40,
+                                     TestLabel "WireAllocMap_Init_4" test41,
+                                     TestLabel "WireAllocMap_Init_5" test42,
+                                     TestLabel "WireAllocMap_Alloc_1" test43,
+                                     TestLabel "WireAllocMap_Alloc_2" test44,
+                                     TestLabel "WireAllocMap_Alloc_3" test45,
+                                     TestLabel "WireAllocMap_Alloc_4" test46,
+                                     TestLabel "WireAllocMap_Alloc_5" test47,
+                                     TestLabel "WireAllocMap_Alloc_6" test48,
+                                     TestLabel "WireAllocMap_Alloc_7" test49,
+                                     TestLabel "WireAllocMap_Alloc_8" test50,
+                                     TestLabel "WireAllocMap_Alloc_9" test51,
+                                     TestLabel "WireAllocMap_Alloc_10" test52,
+                                     TestLabel "WireAllocMap_Alloc_11" test53,
+                                     TestLabel "WireAllocMap_Alloc_12" test54,
+                                     TestLabel "WireAllocMap_Alloc_13" test55,
+                                     TestLabel "WireAllocMap_Alloc_14" test56,
+                                     TestLabel "WireAllocMap_Alloc_15" test57,
+                                     TestLabel "WireAllocMap_Alloc_16" test58,
+                                     TestLabel "WireAllocMap_Alloc_17" test59,
+                                     TestLabel "WireAllocMap_Alloc_18" test60,
+                                     TestLabel "WireAllocMap_Alloc_19" test61,
+                                     TestLabel "WireAllocMap_Alloc_20" test62,
+                                     TestLabel "WireAllocMap_Unique_1" test63,
+                                     TestLabel "WireAllocMap_Unique_2" test64,
+                                     TestLabel "WireAllocMap_Unique_3" test65,
+                                     TestLabel "WireAllocMap_Unique_4" test66,
+                                     TestLabel "WireAllocMap_Unique_5" test67,
+                                     TestLabel "WireAllocMap_Unique_6" test68]
 
 main = defaultMain tests
