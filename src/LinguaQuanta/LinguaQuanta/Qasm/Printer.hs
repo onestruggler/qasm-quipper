bc8345f7221f1b742d34e39d4a9325ca82e74305
scottwe <aswesley@uwaterloo.ca>
2023-02-26 11:35:22 -0400

    Printing OpenQASM library deps

--- src/LinguaQuanta/LinguaQuanta/Qasm/Printer.hs
+++ src/LinguaQuanta/LinguaQuanta/Qasm/Printer.hs
@@ -17,13 +17,51 @@ import LinguaQuanta.Qasm.Gate
   , Operand(..)
   , Sign(..)
   )
-import LinguaQuanta.Qasm.GateName (GateName(..))
+import LinguaQuanta.Qasm.GateName
+  ( GateName(..)
+  , isBackportGate
+  , isQelib1Gate
+  , isQuipperGate
+  )
 import LinguaQuanta.Qasm.Language (Expr(..))
 import LinguaQuanta.Qasm.Operand
   ( Operand(..)
   , RValue(..)
   , VoidCall(..)
   )
+
+-------------------------------------------------------------------------------
+-- * Library Usage Summarization.
+
+-- | A data structure to track the built-in OpenQASM libraries used by the
+-- program. The libraries are encoded in the following order: qelib1.inc,
+-- bkpgates.inc, quipgates.inc, quipfuncs.inc.
+data LibUsage = LibUsage Bool Bool Bool Bool deriving (Show, Eq)
+
+-- | Associates a line of an OpenQASM program, together with its usage data.
+type LineSummary = (LibUsage, String)
+
+-- | Returns a LibUsage summary with no libraries in use.
+depfree :: LibUsage
+depfree = LibUsage False False False False
+
+-- | Adds qelib1.inc to the list of libraries in use (in OpenQASM 3, this
+-- corresponds to stdgates.inc).
+useQelib1 :: LibUsage -> LibUsage
+useQelib1 (LibUsage _ bkp qpr qfn) = LibUsage True bkp qpr qfn
+
+-- | Adds bkpgates.inc to the list of libraries in use (in OpenQASM 3, this
+-- corresponds to stdgates.inc).
+useBkpgates :: LibUsage -> LibUsage
+useBkpgates (LibUsage _ _ qpr qfn) = LibUsage True True qpr qfn
+
+-- | Adds quipgates.inc to the list of libraries in use.
+useQuipgates :: LibUsage -> LibUsage
+useQuipgates (LibUsage _ bkp _ qfn) = LibUsage True bkp True qfn
+
+-- | Adds quipfuncs.inc to the list of libraries in use.
+useQuipfuncs :: LibUsage -> LibUsage
+useQuipfuncs (LibUsage _ bkp qpr _) = LibUsage True bkp qpr True
 
 -------------------------------------------------------------------------------
 -- * Tools for Legacy Support.
@@ -34,7 +72,7 @@ _INVS_TOOL  = "elim_invs"
 _CTRLS_TOOL = "elim_ctrls"
 
 -- | Produces an error and directs the user to a tool to resolve the error.
-toolError :: String -> String -> String
+toolError :: String -> String -> a
 toolError tool details = error $ details ++ " " ++ line
     where line = "Run " ++ tool ++ " before generating legacy output."
 
@@ -55,6 +93,10 @@ printQHelper False name op = name ++ "(" ++ printOperand op ++ ")"
 printCHelper :: Bool -> String -> String
 printCHelper True  name = inlineError name
 printCHelper False name = name ++ "()"
+
+-- | Prints build-in non-unitary quantum operators.
+printBuiltinHelper :: String -> Operand -> String
+printBuiltinHelper name op = name ++ " " ++ printOperand op
 
 -------------------------------------------------------------------------------
 -- * Expression Printing.
@@ -205,21 +247,43 @@ printName GateU1            = "u1"
 printName GateU2            = "u2"
 printName GateU3            = "u3"
 
+-- | Takes as input a library usage summary and a gate name. If the gate is
+-- provided by a built-in library, then the library is added to the summary and
+-- the new summary is returned. Otherwise, the unchanged summary is returned.
+addGateToSummary :: LibUsage -> GateName -> LibUsage
+addGateToSummary used name
+    | isQelib1Gate name   = useQelib1 used
+    | isBackportGate name = useBkpgates used
+    | isQuipperGate name  = useQuipgates used
+    | otherwise           = used
+
 -- | Takes as input a legacy flag and a gate. The gate is converted to its
 -- syntactic representation. If the legacy flag is false, then modifiers are
 -- expanded. Otherwise, if modifiers are provided, then an error is raised.
-printGate :: Bool -> Gate -> String
-printGate legacy (NamedGate name params operands mods) = mstr ++ body
-    where mstr = printMods legacy mods 
-          nstr = printName name
-          pstr = printParams legacy "" params
-          ostr = printOperands operands
-          body = nstr ++ pstr ++ " " ++ ostr
-printGate legacy (GPhaseGate param operands mods) = mstr ++ body
-    where mstr = printMods legacy mods 
+printGate :: Bool -> LibUsage -> Gate -> LineSummary
+printGate legacy used (NamedGate name params operands mods) = (used', line)
+    where mstr  = printMods legacy mods 
+          nstr  = printName name
+          pstr  = printParams legacy "" params
+          ostr  = printOperands operands
+          body  = nstr ++ pstr ++ " " ++ ostr
+          line  = mstr ++ body
+          used' = addGateToSummary used name
+printGate legacy used (GPhaseGate param operands mods) = (used, line)
+    where mstr = printMods legacy mods
           pstr = printParams legacy "" [param]
           ostr = printOperands operands
           body = "gphase" ++ pstr ++ " " ++ ostr
+          line = mstr ++ body
+
+-- | Takes as input a legacy flag, a library usage summary, a string encoding
+-- of a gate modifier, and a gate. Returns a syntactic representation of the
+-- gate (with the given modifier applied), together with a new line summary
+-- that takes into account the gate.
+printGateStmt :: Bool -> LibUsage -> String -> Gate -> LineSummary
+printGateStmt legacy used mod gate = (used', line)
+    where (used', gatestr) = printGate legacy used gate
+          line             = mod ++ gatestr ++ ";"
 
 -------------------------------------------------------------------------------
 -- * LValue and RValue Printing.
@@ -229,19 +293,61 @@ printGate legacy (GPhaseGate param operands mods) = mstr ++ body
 -- textual representation of the corresponding lvalue, adhering to the legacy
 -- flag.
 printLValue :: String -> Maybe Int -> String
-printLValue id Nothing      = printOperand $ QRef id
-printLValue id (Just index) = printOperand $ Cell id index
+printLValue id Nothing    = printOperand $ QRef id
+printLValue id (Just idx) = printOperand $ Cell id idx
+
+-- | Specializes printQHelper to printRValue.
+printQRv :: Bool -> LibUsage -> String -> Operand -> LineSummary
+printQRv legacy used name op = (used', call)
+    where call  = printQHelper legacy name op
+          used' = useQuipfuncs used
+
+-- | Specializes printCHelper to printRValue.
+printCRv :: Bool -> LibUsage -> String -> LineSummary
+printCRv legacy used name = (used', call ++ ";")
+    where call  = printCHelper legacy name
+          used' = useQuipfuncs used
+
+-- | Specializes printBuiltinHelper to printRValue.
+printBuiltinRv :: LibUsage -> String -> Operand -> LineSummary
+printBuiltinRv used name op = (used, call)
+    where call = printBuiltinHelper name op
 
 -- | Takes as input a legacy flag and an rvalue. Returns a textual
 -- representation of the rvalue, adhering to the legacy flag.
-printRValue :: Bool -> RValue -> String
-printRValue legacy (QuipMeasure op) = printQHelper legacy "QMeas" op
-printRValue legacy QuipCInit0       = printCHelper legacy "CInit0"
-printRValue legacy QuipCInit1       = printCHelper legacy "CInit1"
-printRValue legacy QuipCTerm0       = printCHelper legacy "CTerm0"
-printRValue legacy QuipCTerm1       = printCHelper legacy "CTerm1"
-printRValue legacy QuipCDiscard     = printCHelper legacy "CDiscard"
-printRValue _      (Measure op)     = "measure " ++ printOperand op
+printRValue :: Bool -> LibUsage -> RValue -> LineSummary
+printRValue legacy used (QuipMeasure op) = printQRv legacy used "QMeas" op
+printRValue legacy used QuipCInit0       = printCRv legacy used "CInit0"
+printRValue legacy used QuipCInit1       = printCRv legacy used "CInit1"
+printRValue legacy used QuipCTerm0       = printCRv legacy used "CTerm0"
+printRValue legacy used QuipCTerm1       = printCRv legacy used "CTerm1"
+printRValue legacy used QuipCDiscard     = printCRv legacy used "CDiscard"
+printRValue _      used (Measure op)     = printBuiltinRv used "measure" op
+
+-------------------------------------------------------------------------------
+-- * Call Printing.
+
+-- | Specializes printQHelper to printCall.
+printQCall :: Bool -> LibUsage -> String -> Operand -> LineSummary
+printQCall legacy used name op = (used', call ++ ";")
+    where call  = printQHelper legacy name op
+          used' = useQuipfuncs used
+
+-- | Specializes printBuiltinHelper to printCall.
+printBuiltinCall :: LibUsage -> String -> Operand -> LineSummary
+printBuiltinCall used name op = (used, call ++ ";")
+    where call = printBuiltinHelper name op
+
+-- | Takes as input a legacy flag and an instance of a void call. Returns a
+-- textual representation of the void call, adhering to the legacy flag.
+printCall :: Bool -> LibUsage -> VoidCall -> LineSummary
+printCall legacy used (QuipQInit0 op)   = printQCall legacy used "QInit0" op
+printCall legacy used (QuipQInit1 op)   = printQCall legacy used "QInit1" op
+printCall legacy used (QuipQTerm0 op)   = printQCall legacy used "QTerm0" op
+printCall legacy used (QuipQTerm1 op)   = printQCall legacy used "QTerm1" op
+printCall legacy used (QuipQDiscard op) = printQCall legacy used "QDiscard" op
+printCall _      used (VoidReset op)    = printBuiltinCall used "reset" op
+printCall _      used (VoidMeasure op)  = printBuiltinCall used "measure" op
 
 -------------------------------------------------------------------------------
 -- * Statement Printing.
@@ -281,49 +387,72 @@ printBitDecl legacy len decl = printDecl legacy "creg" "bit" len decl
 -- index into the declaration (e.g., if the declaration is an array), and an
 -- rvalue. Returns a textual representation of assigning the rvalue to the
 -- given index of the declaration, adhering to the legacy flag.
-printAssign :: Bool -> String -> Maybe Int -> RValue -> String
-printAssign True id index rval@(Measure op) = rstr ++ " -> " ++ lstr ++ ";"
-    where lstr = printLValue id index
-          rstr = printRValue True rval
-printAssign True _ _ rval
-    | rstr == "" = error "UNREACHABLE!"
+printAssign :: Bool -> LibUsage -> String -> Maybe Int -> RValue -> LineSummary
+printAssign True used id idx rval@(Measure op) = (used', line)
+    where lstr          = printLValue id idx
+          (used', rstr) = printRValue True used rval
+          line          = rstr ++ " -> " ++ lstr ++ ";"
+printAssign True used _ _ rval
+    | rstr == "" = error "UNREACHABLE!" -- Forces evaluation of printRValue.
     | otherwise  = error "Assignments are not supported in legacy mode."
-    where rstr = printRValue True rval
-printAssign legacy id index rval = lstr ++ " = " ++ rstr ++ ";"
-    where lstr = printLValue id index
-          rstr = printRValue legacy rval
-
--- | Takes as input a legacy flag and an instance of a void call. Returns a
--- textual representation of the void call, adhering to the legacy flag.
-printCall :: Bool -> VoidCall -> String
-printCall legacy (QuipQInit0 op)   = printQHelper legacy "QInit0" op ++ ";"
-printCall legacy (QuipQInit1 op)   = printQHelper legacy "QInit1" op ++ ";"
-printCall legacy (QuipQTerm0 op)   = printQHelper legacy "QTerm0" op ++ ";"
-printCall legacy (QuipQTerm1 op)   = printQHelper legacy "QTerm1" op ++ ";"
-printCall legacy (QuipQDiscard op) = printQHelper legacy "QDiscard" op ++ ";"
-printCall _      (VoidReset op)    = "reset " ++ printOperand op ++ ";"
-printCall _      (VoidMeasure op)  = "measure " ++ printOperand op ++ ";"
+    where (_, rstr) = printRValue True used rval
+printAssign legacy used id idx rval = (used', line)
+    where lstr          = printLValue id idx
+          (used', rstr) = printRValue legacy used rval
+          line          = lstr ++ " = " ++ rstr ++ ";"
 
 -- | Concretizes a statement, and produces its syntactic representation.
-printAstStmt :: Bool -> AstStmt -> String
-printAstStmt legacy (AstGateStmt n gate)
-    | n == 0     = gateStr ++ ";"
-    | not legacy = "pow(" ++ show n ++ ") @ " ++ gateStr ++ ";"
+printAstStmt :: Bool -> LibUsage -> AstStmt -> LineSummary
+printAstStmt legacy used (AstGateStmt n gate)
+    | n == 0     = printGateStmt legacy used "" gate
+    | not legacy = printGateStmt legacy used pmodStr gate
     | otherwise  = toolError _POWS_TOOL errline
-    where gateStr = printGate legacy gate
+    where pmodStr = "pow(" ++ show n ++ ") @ "
           errline = "Power modifiers are not supported in legacy mode."
-printAstStmt legacy (AstQubitDecl len decl)   = printQubitDecl legacy len decl
-printAstStmt legacy (AstBitDecl len decl)     = printBitDecl legacy len decl
-printAstStmt legacy (AstAssign id index rval) = printAssign legacy id index rval
-printAstStmt legacy (AstCall call)            = printCall legacy call
+printAstStmt legacy used (AstQubitDecl len decl) = (used, line)
+    where line = printQubitDecl legacy len decl
+printAstStmt legacy used (AstBitDecl len decl) = (used, line)
+    where line = printBitDecl legacy len decl
+printAstStmt legacy used (AstAssign id idx rval) = res
+    where res = printAssign legacy used id idx rval
+printAstStmt legacy used (AstCall call) = printCall legacy used call
+
+-- | Helper function which maps (printAstStmts legacy) through the list of
+-- statements, while tracking the library usage at each step.
+astToLines :: Bool -> [AstStmt] -> (LibUsage, [String])
+astToLines _ []                = (depfree, [])
+astToLines legacy (stmt:stmts) = (used', line:lines)
+    where (used, lines) = astToLines legacy stmts
+          (used', line) = printAstStmt legacy used stmt
 
 -- | Returns the version string, according to whether the output is legacy.
 printVersion :: Bool -> String
 printVersion legacy = "OPENQASM " ++ vnum ++ ";"
     where vnum = if legacy then "2.0" else "3"
 
+-- | Takes as input a boolean condition, a list of OpenQASM statements (encoded
+-- as strings), and the name of an OpenQASM library. If the boolean condition
+-- is satisfied, then preprends an include statement for the library to the
+-- list. Otherwise, the list is returned unchaged.
+addIncludeIf :: Bool -> [String] -> String -> [String]
+addIncludeIf False lines _   = lines
+addIncludeIf True  lines inc = line:lines
+    where line = "include \"" ++ inc ++ "\";"
+
+-- | Consumes the syntactic representation of an OpenQASM program, and prepends
+-- an include statement for each built-in library used by the program.
+prependIncludes :: Bool -> LibUsage -> [String] -> [String]
+prependIncludes legacy (LibUsage std bkp qpr qfn) lines = tmp5
+    where tmp0 = lines
+          tmp1 = addIncludeIf qfn                          tmp0 "quipfuncs.inc"
+          tmp2 = addIncludeIf qpr                          tmp1 "quipgates.inc"
+          tmp3 = addIncludeIf ((std || bkp) && not legacy) tmp2 "stdgates.inc"
+          tmp4 = addIncludeIf (bkp && legacy)              tmp3 "bkpgates.inc"
+          tmp5 = addIncludeIf (std && legacy)              tmp4 "qelib1.inc"
+
 -- | Concretizes each statement, and produces its syntactic representation.
 printAst :: Bool -> [AstStmt] -> [String]
-printAst legacy stmts = vers:body
-    where body = map (printAstStmt legacy) stmts
-          vers = printVersion legacy
+printAst legacy stmts = vers:bodyWithIncludes
+    where (used, body)     = astToLines legacy stmts
+          bodyWithIncludes = prependIncludes legacy used body
+          vers             = printVersion legacy
